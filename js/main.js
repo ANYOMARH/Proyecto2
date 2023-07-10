20f411642d8ccac4fc50d333c5682cd8337316c0
18159476 <alexander2470@gmail.com>
2023-07-10 08:05:05 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//Flecha inferior derecha
+
+const desplazarArriba = document.querySelector("#desplazarse-hacia-arriba");
+
+desplazarArriba.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+
+//Mensaje POPUP curriculum
+let popup = document.getElementById("popup");
+
+function openpopup(){
+    popup.classList.add("open-popup");
+}
+
+function closepopup(){
+    popup.classList.remove("open-popup");
+}
+
+// Barra de navegación hamburguesa
+const burger = document.querySelector("#menu-hamburguesa");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+// Cerrar el menú de hambuguesa cuando se hace click en un enlace  
+
+// Seleccionar enlaces de navegación
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
